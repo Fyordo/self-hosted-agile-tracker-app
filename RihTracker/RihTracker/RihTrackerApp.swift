ad888f50e7c0863f5b9581a1dde2e02e1165d367
Fedor <lagutinfedya@gmail.com>
2023-12-26 16:16:13 +0300

    Add base files

---
+++ RihTracker/RihTracker/RihTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RihTrackerApp.swift
+//  RihTracker
+//
+//  Created by Лагутин Фёдор on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RihTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
